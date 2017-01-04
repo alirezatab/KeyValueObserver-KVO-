e314c1f7b95099bbe205c7b85798acd72a5e9cad
alirezatab <alirezatabrizi10@gmail.com>
2017-01-03 17:51:39 -0800

    Added the empty project file

---
+++ KeyValueObserver(KVO)/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KeyValueObserver(KVO)
+//
+//  Created by ALIREZA TABRIZI on 1/3/17.
+//  Copyright © 2017 AR-T.com, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
